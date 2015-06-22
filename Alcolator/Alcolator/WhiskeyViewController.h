e3286d4e22ad5054be78a565e2453024c6f783e6
Rick Matta <rickmatta@gmail.com>
2015-06-22 14:16:36 -0400

    checkpoint 20 complete

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,15 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by rick m on 6/21/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+
+@end
