b23e4951b12c5ea7574b25791329c42104f16d59
Rick Matta <rickmatta@gmail.com>
2015-06-22 15:57:53 -0400

    chkpt 12 completed

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by rick m on 6/22/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
